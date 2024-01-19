b7a0c8ab6de1ffc981ca47815747aec33b809178
Victor <ejenamvictor76@gmail.com>
2024-01-19 17:19:51 +0000

    working code

--- Makefile
+++ Makefile
@@ -3,5 +3,11 @@ install:
 		pip install -r requirements.txt
 test:
 	python -m pytest -vv test_*.py
+
+format:
+	black *.py
+
+lint:
+	pylint --disable=R,C,E1120 wikibot.py
 run:
 	python wikibot.py
--- wikibot.py
+++ wikibot.py
@@ -1,14 +1,15 @@
 from mylib.bot import scrape
 import click
 
+
 @click.command()
-@click.option('--name',
-              help='web page we want to scrape')
-@click.option('--length',
-              help='length of the output from wikipedia')
-def cli(name, length=1):
+@click.option("--name", default="microsoft", help="web page we want to scrape")
+@click.option("--length", default=1, help="length of the output from wikipedia")
+def cli(name, length):
+    # pylint: disable=no-value-for-parameter
     result = scrape(name, length)
     click.echo(click.style(f"{result}", fg="red"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
